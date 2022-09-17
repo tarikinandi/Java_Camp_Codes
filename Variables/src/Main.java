f82014ebc87a78160676888eaa1f23b160827215
Tarık İnandı <muhtark02@hotmail.com>
2022-09-17 10:22:23 +0300

    Camp Codes
    Codes I wrote in classes at Engin Demiroğ Java 2022 Camp.

---
+++ Variables/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int studentCount = 14;
+		String message = "Student Count : ";
+		System.out.println(message + studentCount);
+		System.out.println(message + studentCount);
+		System.out.println("Student Count : " + studentCount);
+		System.out.println("Student Count : " + studentCount);
+		System.out.println("Student Count : " + studentCount);
+	}
+
+}
---
+++ dataTypes/src/dataTypes/Main.java
@@ -0,0 +1,14 @@
+package dataTypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double number = 12.34;
+		number = 12;
+		
+		char character = 'J';
+		
+		boolean trueOrFalse = true;
+	}
+
+}
