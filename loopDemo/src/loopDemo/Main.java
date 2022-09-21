77cc0d310cfd1f99d713ba0194dcd7b2908e5a21
Tarık İnandı <muhtark02@hotmail.com>
2022-09-21 13:18:14 +0300

    Camp Codes
    Codes I wrote in classes at Engin Demiroğ Java 2022 Camp.

---
+++ loopDemo/src/loopDemo/Main.java
@@ -0,0 +1,14 @@
+package loopDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i;
+		
+		for(i = 0 ; i < 64 ; i++) {
+			System.out.println(i);
+		}
+		System.out.println("Loop finished");
+	}
+
+}
