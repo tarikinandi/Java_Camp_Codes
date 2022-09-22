8f50e761180534e01433c43725a602b01f0837b4
Tarık İnandı <muhtark02@hotmail.com>
2022-09-22 13:48:57 +0300

    Camp Codes
    Codes I wrote in classes at Engin Demiroğ Java 2022 Camp.

--- loopDemo/src/loopDemo/Main.java
+++ loopDemo/src/loopDemo/Main.java
@@ -3,12 +3,31 @@ package loopDemo;
 public class Main {
 
 	public static void main(String[] args) {
-		int i;
-		
-		for(i = 0 ; i < 64 ; i++) {
+		//for
+		System.out.println("For loop started!");
+		for(int i = 2 ; i < 75 ; i += 3) {
 			System.out.println(i);
 		}
-		System.out.println("Loop finished");
+		System.out.println("For loop ended!\n");
+		
+		//while
+		System.out.println("While loop started!");
+		int i = 0;
+		
+		while(i < 12) {
+			System.out.println(i);
+			i++;
+		}
+		System.out.println("While loop ended!\n");
+		
+		//do while
+		System.out.println("Do-While loop started!");
+		int j = 75;
+		do {
+			System.out.println(j);
+			j--;
+		} while (j > 15);
+		System.out.println("Do-While loop ended\n");
 	}
 
 }
