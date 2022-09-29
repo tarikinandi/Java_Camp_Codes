ff4f14e11336b591db1b326f884a81140ae35af8
Tarık İnandı <91372783+tarikinandi@users.noreply.github.com>
2022-09-29 10:34:22 +0300

    Update Main.java

--- multiDimensionalArrayDemo/src/multiDimensionalArrayDemo/Main.java
+++ multiDimensionalArrayDemo/src/multiDimensionalArrayDemo/Main.java
@@ -5,15 +5,15 @@ public class Main {
 	public static void main(String[] args) {
 		String[][] citys = new String[3][3];
 
-		citys[0][0] = "Ýstanbul";
+		citys[0][0] = "Istanbul";
 		citys[0][1] = "Kocaeli";
 		citys[0][2] = "Bursa";
-		citys[1][0] = "Ýzmir";
-		citys[1][1] = "Aydýn";
+		citys[1][0] = "Izmir";
+		citys[1][1] = "Aydin";
 		citys[1][2] = "Manisa";
-		citys[2][0] = "Adýyaman";
+		citys[2][0] = "Adiyaman";
 		citys[2][1] = "Gaziantep";
-		citys[2][2] = "Þanlýurfa";
+		citys[2][2] = "Sanliurfa";
 
 		for (int i = 0; i < 3; i++) {
 			System.out.println("-------------------------------------------------------");
